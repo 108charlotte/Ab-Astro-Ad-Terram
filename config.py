f6907e084418835b4e398bf1b775485bf4a01918
108charlotte <108sarahsmith@gmail.com>
2025-07-30 15:23:11 -0400

    prepared for railway deployment

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ from app.instance.config import SECRET_KEY
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config: 
-    SECRET_KEY = SECRET_KEY
+    SECRET_KEY = os.environ.get('SECRET_KEY') or SECRET_KEY
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    DATABASE = os.path.join(BASEDIR, 'app', 'instance', 'game.sqlite')
+    
+    if os.environ.get('DATABASE_URL'):
+        DATABASE = os.environ.get('DATABASE_URL')
+    else:
+        DATABASE = os.path.join(BASEDIR, 'app', 'instance', 'game.sqlite')
